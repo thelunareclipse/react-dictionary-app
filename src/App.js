abe563485e84b17a8d91a163acdc825c016ba017
Luna <69701846+thelunareclipse@users.noreply.github.com>
2023-08-21 11:31:20 -0400

    New Dictionary component and search input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary App</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
