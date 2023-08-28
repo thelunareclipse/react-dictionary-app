9a367fc93a43b44199b8b30635391e3387ae5c96
Luna <69701846+thelunareclipse@users.noreply.github.com>
2023-08-28 17:26:23 -0400

    Implemented synonym display capabilities

---
+++ src/Synonyms.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  console.log(props.synonyms);
+  if (props.synonyms) {
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonyms, index) {
+          return <li key={index}>{synonyms}</li>;
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
